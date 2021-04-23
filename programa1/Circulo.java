c5f1cfb0529de689e71344af65d02c2aac993a6a
DANIELFELIPEF48 <>
2021-04-23 03:02:38 +0000

    circulo

--- programa1/Circulo.java
+++ programa1/Circulo.java
@@ -3,10 +3,22 @@ import javax.swing.JApplet;
 import javax.swing.JFrame;
 
 public class Circulo extends JApplet {
-
+  int i=100, j=70,o1,o2,c;
   public void paint (Graphics g) {
-
-    g.drawString (".", 50, 50);
+    g.drawString (".", 100, 100);//centro
+    for (i=0;i<=200;i++)
+    {
+      for (j=0;j<=200;j++)
+      {
+        o1=(i-100)*(i-100);
+        o2=(j-100)*(j-100);
+        c=o1+o2;
+        if (c==900)
+        {
+          g.drawString (".", i, j);
+        }
+      }
+    }
   }
 
   public static void main (String[] args){
